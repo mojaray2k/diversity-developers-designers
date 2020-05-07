00f6192f1f15d3912c6341ab9a35e1f6e3efcf93
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-07 11:46:02 -0400

    setup login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
+  return (
+    <Fragment>
+      {/* <div class='alert alert-danger'>Invalid credentials</div> */}
+      <h1 class='large text-primary'>Sign In</h1>
+      <p class='lead'>
+        <i class='fas fa-user'></i> Sign into Your Account
+      </p>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            onChange={(e) => onChange(e)}
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            onChange={(e) => onChange(e)}
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={password}
+          />
+        </div>
+        <input
+          onChange={(e) => onChange(e)}
+          type='submit'
+          className='btn btn-primary'
+          value='Login'
+        />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link href='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -20,24 +19,7 @@ const Register = () => {
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      // console.log(formData);
-      const newUser = {
-        name,
-        email,
-        password,
-      };
-      try {
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-        const body = JSON.stringify(newUser);
-        const res = await axios.post("/api/users", body, config);
-        console.log(res.data);
-      } catch (err) {
-        console.error(err.response.data);
-      }
+      console.log("SUCCESS");
     }
   };
 
