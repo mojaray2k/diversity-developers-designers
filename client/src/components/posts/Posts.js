0549a3c3cc193fd885a4915f39050d16e8ecd1bf
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-09 16:10:00 -0400

    added functionality to add a post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import PostForm from "./PostForm";
 import PostItem from "./PostItem";
 import { getPosts } from "../../actions/post";
 
@@ -17,7 +18,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       <p className='lead'>
         <i className='fas fa-user'>Welcome to the community</i>
       </p>
-      {/* Post Form */}
+      <PostForm />
       <div className='posts'>
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
